06526f9aa0dffc58a963ee818c0ff045305a79df
Parisa <20560187+Parisa-lab@users.noreply.github.com>
2026-09-02 12:31:30 +0330

    Update vector.py

--- 01_vectors/vector.py
+++ 01_vectors/vector.py
@@ -1,5 +1,10 @@
 def vector_length(vector):
     return len(vector) 
 
+def access_components(vector):
+    return vector[i]
+
 x = [2, 5, 3]
 print(vector_length(x))
+
+print(vector_components(x[i]))

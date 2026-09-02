42abc0f65e17cbf3e76d57f7c95d8000d4b49eb7
Parisa <20560187+Parisa-lab@users.noreply.github.com>
2026-09-02 12:37:36 +0330

    Update vector.py

--- 01_vectors/vector.py
+++ 01_vectors/vector.py
@@ -3,9 +3,18 @@ def vector_length(vector):
 
 def vector_components(vector, i):
     return vector[i]
+    
+def vector_change_components(vector, i, new):
+    vector[i] = new
+    return vector
 
 x = [2, 5, 3]
 i = 1
+new_num = 10
+
 print(vector_length(x))
 
 print(vector_components(x, i))
+
+x = vector_change_components(x, i, new_num)  
+print(x)

5d9d2510205fef4edd589f79ac08c8e8fa9f599f
Parisa <20560187+Parisa-lab@users.noreply.github.com>
2026-09-05 15:37:45 +0330

    Update vector.py

--- 01_vectors/vector.py
+++ 01_vectors/vector.py
@@ -12,6 +12,15 @@ def vector_add(a, b):
         return [a[i] + b[i] for i in range(len(a))]
     else:
         raise ValueError("Vectors must have the same dimension.")
+        
+def vector_subtract(a, b):
+    if len(a) == len(b):
+        return [a[i] - b[i] for i in range(len(a))]
+    else:
+        raise ValueError("Vectors must have the same dimension.")
+        
+def vector_scalar_multiply(vector, scalar):
+    return [scalar * vector[i] for i in range(len(a))]    
 
 x = [3, 6, 9]
 i = 1
@@ -28,4 +37,7 @@ print("Updated vector is:", x)
 
 a = [3, 50, 9]
 b = [1, 2, 4]
+scalar = 3
 print("Sum:", vector_add(a, b))
+print("Subtraction:", vector_subtract(a, b))
+print("Scalar multiplication:", vector_scalar_multiply(a, scalar))
